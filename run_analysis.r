4566edd8c54b88d8f4ef6581421ddb3401a2ec70
Mohammed Ali <moh_fcis@yahoo.com>
2016-02-20 15:30:30 +0200

    filter attriburtes

--- run_analysis.r
+++ run_analysis.r
@@ -13,3 +13,11 @@ product_training_test<- full_join(products_training,products_test)
 
 #combine test data and data combined at the previous steps,Joining by: c("product_uid")
 product_all <- right_join(product_training_test, product_description)
+
+#Read the attributes of products
+products_attributes <- tbl_df(read.csv("data/attributes.csv",stringsAsFactors = FALSE, na.strings = 'N/A'))
+
+
+products_attributes <- products_attributes  %>%
+                       filter(product_uid != 'NA') %>% #revmove null rows
+                       distinct(product_uid, name) #remove duplicates
