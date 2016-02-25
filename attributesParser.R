ef95ed011f668bb8c9a0f9412876fdb2b754d00f
@LiZaT <ali_ezzat85@yahoo.com>
2016-02-25 20:12:34 +0800

    Update attributesParser.R
    A separate function was written for each column (feature) that is intended to be created later:
    - bulletsParser --> parses the attributes for "bullet" attributes
    - yesesParser --> parses the attributes that have "Yes" as a value
    - nosParser --> ...
    - keysParser --> ...
    - valuesParser --> ...

--- attributesParser.R
+++ attributesParser.R
@@ -22,42 +22,138 @@ allProductsAttributesParser <- function(all_products_attributes) {
 
 #-----------------------------------------------------------------
 
-## Obtains the values for the new attribute fields for single product
-singleProductAttributesParser <- function(single_product_attributes) {
+attributesSplitter <- function(single_product_attributes) {
   
   ## split attributes from one another
   single_product_attributes <- single_product_attributes %>%  ## a single string containing all attributes for a single product
                                strsplit(split = "@@@@") %>%   ## a list where each element is a an attribute of the product
                                as.data.frame()                ## a column vector (of type 'dataframe')
-
+  
   ## give the only column in the data frame a name for later access
   colnames(single_product_attributes) <- "attr_key_val"
-
+  
   ## separate the only column in the data frame into 2 columns: "key" & "value"
   single_product_attributes <- separate(single_product_attributes, col = "attr_key_val", remove = TRUE, sep = ";;", into = c('key','value'))
 
-  ## BULLETS
-  bullets_rows <- single_product_attributes[startsWith(single_product_attributes$key, "Bullet", ignore.case = TRUE, trim = TRUE),]
-  bullets <- paste(bullets_rows$value, collapse = " ")
-
-  ## YES
-  yeses_rows <- single_product_attributes[startsWith(single_product_attributes$value, "Yes", ignore.case = TRUE, trim = TRUE),]
-  yeses <- paste(yeses_rows$key, collapse = " ")
-
-  ## NO
-  nos_rows <- single_product_attributes[startsWith(single_product_attributes$value, "No", ignore.case = TRUE, trim = TRUE),]
-  nos <- paste(nos_rows$key, collapse = " ")
-  
-  ## REMAINING
-  remaining_rows <- subset(single_product_attributes, !(key %in% rbind(bullets_rows, yeses_rows, nos_rows)$key))
-  keys <- paste(remaining_rows$key, collapse = " ")
-  values <- paste(remaining_rows$value, collapse = " ")
-
-  result <- data.frame(bullets, yeses, nos, keys, values)
-
-  result
+  single_product_attributes
 
 }
+
+#-----------------------------------------------------------------
+
+bulletsParser <- function(single_product_attributes) {
+
+  ## split attributes from one another
+  single_product_attributes <- attributesSplitter(single_product_attributes)
+  
+  ## BULLETS
+  bullets_rows <- single_product_attributes[startsWith(single_product_attributes$key, "Bullet"),]
+  bullets <- paste(bullets_rows$value, collapse = "")
+
+  bullets
+
+}
+
+#-----------------------------------------------------------------
+
+yesesParser <- function(single_product_attributes) {
+  
+  ## split attributes from one another
+  single_product_attributes <- attributesSplitter(single_product_attributes)
+  
+  ## YES
+  yeses_rows <- single_product_attributes[startsWith(single_product_attributes$value, "Yes"),]
+  yeses <- paste(yeses_rows$key, collapse = "")
+  
+  yeses
+  
+}
+
+#-----------------------------------------------------------------
+
+nosParser <- function(single_product_attributes) {
+  
+  ## split attributes from one another
+  single_product_attributes <- attributesSplitter(single_product_attributes)
+  
+  ## NO
+  nos_rows <- single_product_attributes[startsWith(single_product_attributes$value, "No"),]
+  nos <- paste(nos_rows$key, collapse = "")
+  
+  nos
+  
+}
+
+#-----------------------------------------------------------------
+
+keysParser <- function(single_product_attributes) {
+  
+  ## split attributes from one another
+  single_product_attributes <- attributesSplitter(single_product_attributes)
+  
+  ## all the rows that will be excluded
+  bullets_rows <- single_product_attributes[startsWith(single_product_attributes$key, "Bullet"),]
+  yeses_rows <- single_product_attributes[startsWith(single_product_attributes$value, "Yes"),]
+  nos_rows <- single_product_attributes[startsWith(single_product_attributes$value, "No"),]
+  
+  ## rows to be kept
+  remaining_rows <- subset(single_product_attributes, !(key %in% rbind(bullets_rows, yeses_rows, nos_rows)$key))
+
+  ## KEY
+  keys <- paste(remaining_rows$key, collapse = "")
+
+}
+
+#-----------------------------------------------------------------
+
+valuesParser <- function(single_product_attributes) {
+  
+  ## split attributes from one another
+  single_product_attributes <- attributesSplitter(single_product_attributes)
+  
+  ## all the rows that will be excluded
+  bullets_rows <- single_product_attributes[startsWith(single_product_attributes$key, "Bullet"),]
+  yeses_rows <- single_product_attributes[startsWith(single_product_attributes$value, "Yes"),]
+  nos_rows <- single_product_attributes[startsWith(single_product_attributes$value, "No"),]
+  
+  ## rows to be kept
+  remaining_rows <- subset(single_product_attributes, !(key %in% rbind(bullets_rows, yeses_rows, nos_rows)$key))
+  
+  ## KEY
+  values <- paste(remaining_rows$value, collapse = "")
+  
+}
+
+#-----------------------------------------------------------------
+
+# ## Obtains the values for the new attribute fields for single product
+# singleProductAttributesParser <- function(single_product_attributes) {
+#   
+#   ## split attributes from one another
+#   single_product_attributes <- attributesSplitter(single_product_attributes)
+# 
+#   ## BULLETS
+#   bullets_rows <- single_product_attributes[startsWith(single_product_attributes$key, "Bullet"),]
+#   bullets <- paste(bullets_rows$value, collapse = " ")
+# 
+#   ## YES
+#   yeses_rows <- single_product_attributes[startsWith(single_product_attributes$value, "Yes"),]
+#   yeses <- paste(yeses_rows$key, collapse = " ")
+# 
+#   ## NO
+#   nos_rows <- single_product_attributes[startsWith(single_product_attributes$value, "No"),]
+#   nos <- paste(nos_rows$key, collapse = " ")
+#   
+#   ## REMAINING
+#   remaining_rows <- subset(single_product_attributes, !(key %in% rbind(bullets_rows, yeses_rows, nos_rows)$key))
+#   keys <- paste(remaining_rows$key, collapse = " ")
+#   values <- paste(remaining_rows$value, collapse = " ")
+# 
+#   result <- data.frame(bullets, yeses, nos, keys, values)
+# 
+#   result
+# 
+# }
 
 #-----------------------------------------------------------------
   
